bd7e034864cd373f8366207e4bd009ab9d639473
Daniel Araújo <daniel.ti.info@gmail.com>
2024-08-31 01:57:28 -0300

    Ensuring good formatting of the sql query

--- src/services/listService.ts
+++ src/services/listService.ts
@@ -2,12 +2,13 @@ import { executeQuery } from '../database/db';
 
 export async function listMeasures(customer_code: string, measure_type?: string) {
     let query
-        = `SELECT measure_uuid, measure_datetime, measure_type, has_confirmed, image,
+        = `SELECT measure_uuid, measure_datetime, measure_type, has_confirmed, image
      FROM measures WHERE customer_code = ?`;
     const params: (string | boolean)[] = [customer_code];
 
     if (measure_type) {
-        if (measure_type.toUpperCase() !== 'WATER' && measure_type.toUpperCase() !== 'GAS') {
+        const upperMeasureType = measure_type.toUpperCase();
+        if (upperMeasureType !== 'WATER' && upperMeasureType !== 'GAS') {
             throw new Error('INVALID_TYPE');
         }
         query += ` AND measure_type = ?`;
